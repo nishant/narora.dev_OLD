869204137bf4bed05e330f4e82e5fdc37b52d97d
nishant <nish54@gmail.com>
2020-05-15 23:38:20 -0500

    css changes

--- src/app/animations/slide.animation.ts
+++ src/app/animations/slide.animation.ts
@@ -16,7 +16,7 @@ export const slideInAnimation =
       group([
         query(':enter', [
           style({ transform: 'translateX(-100%)' }),
-          animate('1s ease-in-out', style({ transform: 'translateX(0%)' }))
+          animate('.7s ease-in-out', style({ transform: 'translateX(0%)' }))
         ], { optional: true })
       ])
     ])]);
