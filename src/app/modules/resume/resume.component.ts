5e8f3c31eed796edb9b3ba8b68a4cbaa578ec4b7
nishant <nish54@gmail.com>
2020-05-16 03:43:23 -0500

    set up grid layout for resume page

--- src/app/modules/resume/resume.component.ts
+++ src/app/modules/resume/resume.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
+  tiles: Tile[] = [
+    {text: 'One', cols: 2, rows: 3, color: '#00b4d8'},
+    {text: 'Two', cols: 2, rows: 6, color: '#86c2d6'},
+    {text: 'Three', cols: 2, rows: 2, color: '#90e0ef'},
+    {text: 'Four', cols: 2, rows: 1, color: '#00a6e6'},
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
+
+// color palette
+// 137098
+// 1c6789
+// 1F4068
+// 1F4068
+
--- src/app/modules/resume/resume.module.ts
+++ src/app/modules/resume/resume.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResumeComponent } from './resume.component';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 
 @NgModule({
   declarations: [ResumeComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        MatGridListModule
+    ]
 })
 export class ResumeModule { }
